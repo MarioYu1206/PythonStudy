2bc70682c91a054cab48ea5cefb5cb3a8fb04abd
MarioYu1206 <yujunlongoujie@163.com>
2019-04-04 22:14:47 +0800

    created

---
+++ Chapter3/3.4_list_index.py
@@ -0,0 +1,6 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+#print(motorcycles[3]) # list index out of range
+print(motorcycles[-1])
+
+motorcycles = []
+#print(motorcycles[-1]) # list index out of range
