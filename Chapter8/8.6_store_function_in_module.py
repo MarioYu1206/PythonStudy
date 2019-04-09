38f80ffebc526a8f7f8f2ecf3f912a774d578e9a
MarioYu1206 <yujunlongoujie@163.com>
2019-04-09 23:19:07 +0800

    created

--- Chapter8/8.6_store_function_in_module.py
+++ Chapter8/8.6_store_function_in_module.py
@@ -1,3 +1,25 @@
 #函数的优点之一是可将代码块与主程序分离
-import pizza
+#导入整个模块
+import pizza #貌似模块不能以数字开头来命名。。。
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms','green peppers','extra cheese')
 
+#导入特定函数
+from pizza import make_pizza
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms','green peppers','extra cheese')
+
+#使用as给函数指定别名
+from pizza import make_pizza as mp
+mp(16, 'pepperoni')
+mp(12, 'mushrooms','green peppers','extra cheese')
+
+#使用as给模块指定别名
+import pizza as p
+p.make_pizza(16, 'pepperoni')
+p.make_pizza(12, 'mushrooms','green peppers','extra cheese')
+
+#导入模块中的所有函数
+from pizza import *
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms','green peppers','extra cheese')
