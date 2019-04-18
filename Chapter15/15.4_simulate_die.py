e842def3bc6ceb59ab7f51958a2845d97f84be7c
MarioYu1206 <yujunlongoujie@163.com>
2019-04-18 19:28:13 +0800

    updated

--- Chapter15/15.4_simulate_die.py
+++ Chapter15/15.4_simulate_die.py
@@ -31,3 +31,78 @@ hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 hist.add('D6',frequencies) #我们使用add() 将一系列值添加到图表中（向它传递要给添加的值指定的标签， 还有一个列表， 其中包含将出现在图表中的值） 。
 hist.render_to_file('die_visual.svg')
+
+#同时掷两个面数不同骰子
+die1 = Die()
+die2 = Die()
+
+results = []
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll()
+    results.append(result)
+
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+for value in range(2,max_result):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+hist = pygal.Bar()
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = [ '2', '3', '4', '5', '6','7','8','9','10','11','12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6',frequencies)
+hist.render_to_file('dice_visual_2.svg')
+
+
+#同时掷两个骰子
+die1 = Die()
+die2 = Die(10)
+
+results = []
+for roll_num in range(50000):
+    result = die1.roll() + die2.roll()
+    results.append(result)
+
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+for value in range(2,max_result):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+hist = pygal.Bar()
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = [ '2', '3', '4', '5', '6','7','8','9','10','11','12','13','14','15','16']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6',frequencies)
+hist.render_to_file('dice_visual_3.svg')
+
+
+#练习
+#practice 1
+die1 = Die()
+die2 = Die()
+
+results = []
+for roll_num in range(1000):
+    result = die1.roll() + die2.roll()
+    results.append(result)
+
+frequencies = []
+max_result = die1.num_sides + die2.num_sides
+for value in range(2,max_result):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+hist = pygal.Bar()
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = [ x for x in range(2,17)]
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6 + D6',frequencies)
+hist.render_to_file('dice_visual_2.svg')
